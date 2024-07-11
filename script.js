8db4392186c47632c7568f5fb71561975cad1473
Glitch (glitch-hello-website) <none>
2024-07-11 14:40:48 +0000

    🐔🚲 Checkpoint ./script.js:65262463/644

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const slides = document.getElementsById("mySlides");
+const jacky = [
+  [''],
+  []
+];
 
 function print(jacky) {
   for(let i = 0; i < jacky[i]; i++) {
-    slides.
+    const h3 = document.createElement('h3');
+    h3.className = 'insta';
+
+    // Créer la balise a
+    const a = document.createElement('a');
+    a.href = './image.png';
+    a.target = '_blank';
+    a.textContent = 'Texte';
+
+    // Ajouter la balise a à h3
+    h3.appendChild(a);
+
+    // Créer la balise img
+    const img = document.createElement('img');
+    img.className = 'border';
+    img.src = './image.png';
+    img.alt = '';
+
+    // Ajouter les éléments créés au div mySlides
+    const mySlides = document.getElementById('mySlides');
+    mySlides.appendChild(h3);
+    mySlides.appendChild(img);
   }
 }
 
