9c59210a75776bc000e925389371f8d4ec5e5566
Alfakynz <141228129+Alfakynz@users.noreply.github.com>
2024-09-11 21:57:33 +0200

    Add images and bug fixes
    Change CSS (footer not align)
    Add all Jacky's images

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const container = document.getElementById('slideshow-container');
+
 const jacky = [
-  ['https://instagram.com/jacky_du77?igshid=YzcxN2Q2NzY0OA==', 'Instagram de Jacky', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky.jpeg?v=1720706981820'],
-  ['https://instagram.com/jacky_du77?igshid=YzcxN2Q2NzY0OA==', 'Instagram de Jacky', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-tete.png?v=1720706978194'],
-  ['https://www.instagram.com/p/Cuag-QOIXh4/?igshid=YzcxN2Q2NzY0OA==', 'Jacky Célèbre', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-celebre.jpeg?v=1720706951866'],
-  ['https://www.instagram.com/p/CucxUk9r02B/?igshid=YzcxN2Q2NzY0OA==', 'Jacky Flic', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-flic.jpeg?v=1720706953088'],
-  ['https://www.instagram.com/p/CuenVOtorZs/?igshid=YzcxN2Q2NzY0OA==', 'Jacky Boxeur', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-boxeur.jpeg?v=1720706887608'],
-  ['https://www.instagram.com/p/Cuj35_VIhun/?igshid=YmM0MjE2YWMzOA==', 'Jacky Sparrow', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-pirate.jpeg?v=1720706958281'],
-  ['https://www.instagram.com/p/CuuefRDNZMo/?igshid=MzRlODBiNWFlZA==', 'Jacky au McDo', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-mcdo.jpeg?v=1720706954031'],
-  ['https://www.instagram.com/p/CvKWbyKKZCU/?igshid=MzRlODBiNWFlZA==', 'Jacky Soldat', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-soldat.jpeg?v=1720706962328'],
-  ['', 'Jacky Militaire', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-militaire.jpeg?v=1720706955079'],
-  ['', 'Jacky Noël', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-noel.jpeg?v=1720706956199'],
-  ['', 'Jacky Fan de l\'OM', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-om.jpeg?v=1720706957445'],
-  ['', 'Jacky Pompier', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-pompier.jpeg?v=1720706959347'],
-  ['', 'Jacky Rebelle', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-rebelle.jpeg?v=1720706960647'],
-  ['', 'Jacky Rockeur', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-rockeur.jpeg?v=1720706961449'],
-  ['', 'Jacky Vampire', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/jacky-vampire.jpeg?v=1720706979319'],
-  ['', 'Maître Jacky', 'https://cdn.glitch.global/45f09deb-da0b-4a2a-81b3-5e8ced722080/maitre-jacky.jpeg?v=1720706983005']
+  ['https://instagram.com/jacky_du77?igshid=YzcxN2Q2NzY0OA==', 'Instagram de Jacky', 'images/jacky.jpeg'],
+  ['https://instagram.com/jacky_du77?igshid=YzcxN2Q2NzY0OA==', 'Instagram de Jacky', 'images/jacky-tete.png'],
+  ['https://www.instagram.com/p/Cuag-QOIXh4/?igshid=YzcxN2Q2NzY0OA==', 'Jacky Célèbre', 'images/jacky-celebre.jpeg'],
+  ['https://www.instagram.com/p/CucxUk9r02B/?igshid=YzcxN2Q2NzY0OA==', 'Jacky Flic', 'images/jacky-flic.jpeg'],
+  ['https://www.instagram.com/p/CuenVOtorZs/?igshid=YzcxN2Q2NzY0OA==', 'Jacky Boxeur', 'images/jacky-boxeur.jpeg'],
+  ['https://www.instagram.com/p/Cuj35_VIhun/?igshid=YmM0MjE2YWMzOA==', 'Jacky Sparrow', 'images/jacky-pirate.jpeg'],
+  ['https://www.instagram.com/p/CuuefRDNZMo/?igshid=MzRlODBiNWFlZA==', 'Jacky au McDo', 'images/jacky-mcdo.jpeg'],
+  ['https://www.instagram.com/p/CvKWbyKKZCU/?igshid=MzRlODBiNWFlZA==', 'Jacky Soldat', 'images/jacky-soldat.jpeg'],
+  ['', 'Jacky Militaire', 'images/jacky-militaire.jpeg'],
+  ['', 'Jacky Noël', 'images/jacky-noel.jpeg'],
+  ['', 'Jacky Fan de l\'OM', 'images/jacky-om.jpeg'],
+  ['', 'Jacky Pompier', 'images/jacky-pompier.jpeg'],
+  ['', 'Jacky Rebelle', 'images/jacky-rebelle.jpeg'],
+  ['', 'Jacky Rockeur', 'images/jacky-rockeur.jpeg'],
+  ['', 'Jacky Vampire', 'images/jacky-vampire.jpeg'],
+  ['', 'Maître Jacky', 'images/maitre-jacky.jpeg']
 ];
 
 function print(jacky) {
